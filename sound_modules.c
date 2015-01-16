2b9ad8f526d5b91e359c768d169987b2f0631262
Richard Carruthers <rc1024@york.ac.uk>
2015-01-16 09:57:10 +0000

    setup triangle wave

--- sound_modules.c
+++ sound_modules.c
@@ -11,7 +11,7 @@ void generate_sine(int *buf, double amplitude) {
 }
 
 void generate_square(int *buf, double amplitude) {
-    amplitude = amplitude * 309.1;
+    amplitude = amplitude * 402;
     int resolution = 360;
 
     int i;
@@ -25,7 +25,7 @@ void generate_square(int *buf, double amplitude) {
 }
 
 void generate_sawtooth(int *buf, double amplitude) {
-    amplitude = amplitude * 309.1;
+    amplitude = amplitude * 155.0;
     int resolution = 360;
 
     int i;
@@ -38,7 +38,7 @@ void generate_sawtooth(int *buf, double amplitude) {
     }
 }
 
-void generate_triangle(int *bug, double amplitude){
+void generate_triangle(int *buf, double amplitude){
     amplitude = amplitude * 309.1;
     int resolution = 360;
 
--- synth.c
+++ synth.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 #include "lpc_types.h"
 #include "lpc17xx_pinsel.h"
@@ -9,15 +10,16 @@
 #include "sound_modules.h"
 
 int current_tick = 0;
-int *wave_buff;
+int *wave_buf;
 int duration_passed = 0;
+int resolution = 360;
 
 void SysTick_Handler(void) {
     if (current_tick >= 360) {
         current_tick = 0;
     }
 
-    DAC_UpdateValue(LPC_DAC, sine_buff[current_tick]);
+    DAC_UpdateValue(LPC_DAC, wave_buf[current_tick]);
     current_tick += 5;
     duration_passed++;
 }
@@ -52,17 +54,21 @@ double get_freq(int key_n){
 int main(void) {
     init_dac();
     wave_buf = (int *) calloc (resolution, sizeof(int));
-    generate_sine(wave_buff, 2);
+    generate_sine(wave_buf, 2);
     int i;
     double freq;
 
     while(1) {
-        for (i = 40; i < 88; i++) {
-            freq = get_freq(i);
-            wave(freq);
-            while (duration_passed != 5000);
-            duration_passed = 0;
-        }
+        //for (i = 40; i < 88; i++) {
+        //    freq = get_freq(i);
+        //    wave(freq);
+        //    while (duration_passed != 5000);
+        //    duration_passed = 0;
+        //}
+        freq = get_freq(49);
+        wave(freq);
+        while (duration_passed != 5000);
+        duration_passed = 0;
     }
 
     return 0;
