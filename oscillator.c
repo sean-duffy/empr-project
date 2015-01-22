351610197533da38242b51a0730f26bf5e72b401
Richard Carruthers <rc1024@york.ac.uk>
2015-01-22 11:20:54 +0000

    Double based buffer and the removal of amplitude

--- oscillator.c
+++ oscillator.c
@@ -1,70 +1,59 @@
 #include <math.h>
 
-float point_sine(float point, float offset){
-    point = fmod((point+offset),1.0) * 2*M_PI;
+// Wave point generators
+double point_sine(double point, double offset){
+    point = fmod((point + offset),1.0) * 2*M_PI;
     return sin(point);
 }
 
-float point_sawtooth(float point, float offset){
-    return fmod((point+offset+0.5), 1.0);
+double point_sawtooth(double point, double offset){
+    return ((2*fmod((point + offset + 0.25), 1.0)) - 1);
 }
 
-float point_square(float point, float offset){
-    point = fmod((point+offset),1.0);
+double point_square(double point, double  offset){
+    point = fmod((point + offset),1.0);
 
     if (point <0.5){
         return 1;
     } else {
-        return 0;
+        return -1;
     }
 }
 
-float point_triangle(float point, float offset){
-    point = fmod((point+offset + 0.5),1.0);
+double point_triangle(double point, double  offset){    
+    point = fmod((point + offset + 0.25),1.0);
 
     if (point < 0.5){
-        return ((point*2) - 1);
+        return (-1 + (point*4));
     } else {
-        return (1 - (point * 2));
+        return (1 - ((point -0.5) * 4));
     }
 }
 
-void generate_sine(int *buf, double amplitude) {
-    amplitude = amplitude * 380;
-    int resolution = 360;
-
+// Generators
+void generate_sine(double *buf, int res){
     int i;
-    for(i = 0; i < resolution; i++) {
-        buf[i] = (int) floor((amplitude * point_sine((float) i/resolution, 0) + amplitude) / 2);
+    for(i = 0; i < res; i++) {
+        buf[i] = point_sine((double) i/res, 0);
     }
 }
 
-void generate_sawtooth(int *buf, double amplitude) {
-    amplitude = amplitude * 380;
-    int resolution = 360;
-
+void generate_square(double *buf, int res){
     int i;
-    for(i = 0; i < resolution; i++) {
-        buf[i] = (int) floor((amplitude * point_sawtooth((float) i/resolution, 0) + amplitude) / 2);
+    for(i = 0; i < res; i++) {
+        buf[i] = point_square((double) i/res, 0);
     }
 }
 
-void generate_square(int *buf, double amplitude) {
-    amplitude = amplitude * 402;
-    int resolution = 360;
-
+void generate_sawtooth(double *buf, int res){
     int i;
-    for(i = 0; i < resolution; i++) {
-        buf[i] = (int) floor((amplitude * point_square((float) i/resolution, 0) + amplitude) / 2);
+    for(i = 0; i < res; i++) {
+        buf[i] = point_sawtooth((double) i/res, 0);
     }
 }
-
-void generate_triangle(int *buf, double amplitude) {
-    amplitude = amplitude * 380;
-    int resolution = 360;
-
+void generate_triangle(double *buf, int res){
     int i;
-    for(i = 0; i < resolution; i++) {
-        buf[i] = (int) floor((amplitude * point_triangle((float) i/resolution, 0) + amplitude) / 2);
+    for(i = 0; i < res; i++) {
+        buf[i] = point_triangle((double) i/res, 0);
     }
 }
