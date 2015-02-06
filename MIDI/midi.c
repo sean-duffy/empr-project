a7134443aaba2cd749845161521c8eeca17b95c6
Richard Carruthers <rc1024@york.ac.uk>
2015-02-06 11:51:23 +0000

    and even more

---
+++ MIDI/midi.c
@@ -0,0 +1,112 @@
+#include "LPC17xx.h"
+#include "lpc17xx_pinsel.h"
+#include "lpc17xx_can.h"
+
+#include "midi.h"
+#include "UART/uart.h"
+#include <string.h>
+
+#define debug_print(n, x) if(debug) { write_serial(n, x); write_serial("\n\r", 2); }
+#define debug_print_nnl(n, x) if(debug) { write_serial(n, x); }
+
+void interpret_message(CAN_MSG_Type* received_message, uint8_t debug)
+{
+    if(received_message->len == 0)
+    {
+        if ((received_message->id >> 25) == 1)
+        {
+            debug_print("Start", 5);
+        } 
+
+        else if ((received_message->id >> 24) == 1)
+        {
+            debug_print("\n\rEnd", 5);
+        }      
+    }
+
+    else if (received_message->len == 5)
+    {
+        //music data return
+        debug_print_nnl("n", 2);
+        /*struct CAN_return_data music_data = 
+        {
+            {
+                received_message->dataA[0],
+                received_message->dataA[1],
+                received_message->dataA[2],
+                received_message->dataA[3],
+                received_message->dataB[0]
+            },
+            {},
+            1
+        }*/
+        //return music_data;
+    }
+    
+    /*else if(received_message->len == 8)
+    {
+        
+        uint32_t text_da passing argument 1 of 'write_serial' makes pointer from ita;
+        int suc_len;
+
+        int i;
+        char text_message_A[4];
+        for(i = 0; i < 4; i++)
+        {
+            text_message_A[i] = received_message->dataA[i];
+            //suc_len = sprintf(a_print, "%c", text_message);
+            debug_print_nnl(text_message_A, 5);
+        }
+
+        char text_message_B[4];
+        for(i = 0; i < 4; i++)
+        {
+            text_message_B[i] = received_message->dataB[i];
+            //suc_len = sprintf(a_print, "%c", text_message);
+            debug_print_nnl(text_message_B, 5);
+        }
+    }*/
+
+    else if(received_message->len == 8)
+        {
+            char a_print[30];
+            char b_print[30];
+            char concat[100];
+            int charCount = 0;
+            int len_A = 0;
+            int len_B = 0;
+            
+            len_A = sprintf(a_print, "%c", (char)received_message->dataA[0]);
+            charCount += len_A;
+            strcpy(concat, a_print);
+
+            int i;
+            for(i = 1; i < 4; i++){
+                len_A = sprintf(a_print, "%c", (char)received_message->dataA[i]);
+                strcat(concat, a_print);
+                //debug_print_nnl(a_print, len_A);
+                charCount += len_A;
+            }
+
+            for(i = 0; i < 4; i++){
+                len_B = sprintf(b_print, "%c", (char)received_message->dataB[i]);
+                strcat(concat, b_print);
+                //debug_print_nnl(b_print, len_B);
+                charCount += len_B;
+            }
+    
+            /*struct CAN_return_data inst_data = 
+            {
+                {},
+                {
+                    
+                },
+                0
+            }*/
+
+            //strcpy(concat, a_print);
+            //strcat(concat, b_print);
+            debug_print_nnl(concat, charCount);
+        
+        }
+}
--- main.c
+++ main.c
@@ -2,13 +2,12 @@
 #include "lpc17xx_pinsel.h"
 #include "lpc17xx_can.h"
 
+
 #include "CAN/can.h"
 #include "UART/uart.h"
-#include "SYNTH/oscillator.h"
-#include "SYNTH/synth.h"
+#include "MIDI/midi.h"
 
 CAN_MSG_Type RXMsg;
-struct Voice voice_1;
 
 void CAN_IRQHandler(void)
 {
@@ -16,50 +15,8 @@ void CAN_IRQHandler(void)
 
     if((IntStatus>>0)&0x01)
     {
-       CAN_ReceiveMsg(LPC_CAN2, &RXMsg);
-       if(RXMsg.len == 0) { 
-            //write_serial("Received text S/E\n\r", 19);
-        } else if (RXMsg.len == 5) {
-            uint8_t channel = RXMsg.dataA[0];
-            uint8_t note = RXMsg.dataA[1];
-            uint8_t volume = RXMsg.dataA[2];
-            uint8_t type = RXMsg.dataA[3];
-            uint8_t control = RXMsg.dataB[0];
-            char toPrint[50];
-
-            if (channel == 2) {
-                if (volume == 0) {
-                    rest(0);
-                } else {
-                    play_note(voice_1, get_freq(note), 500);
-                }
-
-                //write_serial("Received MIDI\n\r", 15);
-
-                //int recValLength = sprintf(toPrint, "Channel: %d\r\n", channel);      
-                //write_serial(toPrint, recValLength);
-
-                //recValLength = sprintf(toPrint, "Note: %d\r\n", note);
-                //write_serial(toPrint, recValLength);
-
-                //recValLength = sprintf(toPrint, "Volume: %d\r\n", volume);
-                //write_serial(toPrint, recValLength);
-
-                //recValLength = sprintf(toPrint, "Type: %d\r\n", type);
-                //write_serial(toPrint, recValLength);
-
-                //recValLength = sprintf(toPrint, "Control: %d\r\n", control);
-                //write_serial(toPrint, recValLength);
-
-                //write_serial("\r\n", 2);
-            }
-        }
-
-        else if(RXMsg.len == 8) {
-            //write_serial("Received text\r\n", 15);
-        } else {
-            //write_serial("WTF\r\n", 5); 
-        }   
+        CAN_ReceiveMsg(LPC_CAN2, &RXMsg);
+        interpret_message(&RXMsg, 1);
     }
     
 }
@@ -75,30 +32,13 @@ void CAN_InitMessage(void) {
 
 void main()
 {
-    init_dac();
-    int resolution = 360;
-    set_resolution(resolution);
- 
-    double voice_sawtooth[resolution];
-    generate_sawtooth(voice_sawtooth, resolution);
-
-    voice_1.osc_1_buf = voice_sawtooth;
-    voice_1.osc_1_mix = 0.5;
-    voice_1.osc_2_buf = voice_sawtooth;
-    voice_1.osc_2_mix = 0;
-    voice_1.osc_2_detune = 0;
-
-    set_voice(voice_1);
-
-    //play_note(voice_1, 500, 125);
- 
     CAN_InitMessage();
     LPC_GPIO0->FIODIR |= (1 << 10);
- 
+
     LPC_GPIO0->FIOSET &= 0b111111111111111110111111111;
- 
-    //serial_init();
-    //write_serial("booted\n\r", 8);
+
+    serial_init();
+    write_serial("\r\n\r\nbooted\r\n", sizeof("\r\n\r\nbooted\r\n"));
     PINSEL_CFG_Type PinCfg;
     PinCfg.Funcnum = 2;
     PinCfg.OpenDrain = 0;
@@ -109,41 +49,25 @@ void main()
     PinCfg.Pinnum = 5;
     PINSEL_ConfigPin(&PinCfg);
 
-    CAN_Init(LPC_CAN1, 250000);
+    //CAN_Init(LPC_CAN1, 250000);
     CAN_Init(LPC_CAN2, 250000);
 
-    CAN_IRQCmd(LPC_CAN1, CANINT_RIE, ENABLE);
+
+    //CAN_IRQCmd(LPC_CAN1, CANINT_RIE, ENABLE);
     CAN_IRQCmd(LPC_CAN2, CANINT_RIE, ENABLE);
     NVIC_EnableIRQ(CAN_IRQn);
 
     CAN_SetAFMode(LPC_CANAF, CAN_AccBP);
 
-    //write_serial("loaded\n\r", 8);
-    int i = 0, j = 0;
-    char outVal[30];
-
-
-
-    //int n;
-    //double freq;
-
-    //double Cm[] = {40, 43, 47, 52, 55, 59, 64, 67, 71};
-    //double Bb[] = {38, 42, 45, 50, 54, 57, 62, 66, 69};
-    //double Fm[] = {33, 36, 40, 45, 48, 52, 57, 60, 64};
-
-    //double *arps[] = {Cm, Bb, Fm, Cm, Bb, Fm, Cm, Cm};
-
-    //while (1) {
-    //    for (n = 0; n < 8; n++) {
-    //        for (i = 0; i < 9; i++) {
-    //            freq = get_freq(arps[n][i]);
-    //            play_note(voice_1, freq, 125);
-    //        }
-    //        for (i = 7; i > 0; i--) {
-    //            freq = get_freq(arps[n][i]);
-    //            play_note(voice_1, freq, 125);
-    //        }
-    //    }
-    //}
-
+    write_serial("loaded\n\r", 9);
+    
 }
+
+void delay()
+{
+    int i;
+    for (i = 0; i < 9000000; i++);
+}
+
+
+
