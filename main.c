bd39f411c560ce22ac863a2e4f2d220c0e6241d6
Richard Carruthers <rc1024@york.ac.uk>
2015-02-19 12:23:51 +0000

    Changes to test note creation

--- main.c
+++ main.c
@@ -9,16 +9,17 @@
 #include "UART/uart.h"
 #include "SYNTH/oscillator.h"
 #include "SYNTH/synth.h"
+#include "SYNTH/instruments.h"
 #include "MIDI/midi.h"
 
-int debug = 1;
+int debug = 2;
 CAN_MSG_Type RXMsg;
 struct CAN_return_data message;
 
 #define debug_print(n, x) if(debug) { write_serial(n, x); write_serial("\n\r", 2); }
 #define debug_print_nnl(n, x) if(debug) { write_serial(n, x); }
-struct Voice voice_1;
-uint8_t channel_playing = 1;
+
+uint8_t channel_playing = 2;
 
 void CAN_IRQHandler(void) {
     uint8_t IntStatus = CAN_IntGetStatus(LPC_CAN2);
@@ -59,29 +60,35 @@ void CAN_IRQHandler(void) {
 }
 
 void main() {
-    int resolution = 360;
-    set_resolution(resolution);
+    // for debug to work ->> serial_init();
+    
+    set_resolution(360);
  
+    /*
     double wave_buf_1[resolution];
-    generate_sawtooth(wave_buf_1, resolution);
-
+    generate_triangle(wave_buf_1, resolution);
+    
     double wave_buf_2[resolution];
-    generate_square(wave_buf_2, resolution);
+    generate_noise(wave_buf_2, resolution);
+
+    low_pass_filter(wave_buf_1, resolution, 8);
+    low_pass_filter(wave_buf_2, resolution, 8);
 
     voice_1.osc_1_buf = wave_buf_1;
-    voice_1.osc_1_mix = 0.5;
+    voice_1.osc_1_mix = 0.8;
     voice_1.osc_2_buf = wave_buf_2;
-    voice_1.osc_2_mix = 0.5;
+    voice_1.osc_2_mix = 0.2;
     voice_1.osc_2_detune = 0;
-    voice_1.output_attack = 1;
+    voice_1.output_attack = 0.8;
     voice_1.output_release = 0.8;
-    voice_1.envelope_on = 0;
+    voice_1.envelope_on = 1;
 
     init_dac();
     init_can(250000, 0);
+    */
 
-    set_voice(voice_1);
+    set_voice(eggs);
     SysTick_Config(2400);
-
-    while (1);
+    
+    while (1); 
 }
