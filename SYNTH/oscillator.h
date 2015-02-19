5041674530e320b174feb623965b03a3c7892416
Richard Carruthers <rc1024@york.ac.uk>
2015-02-19 12:23:06 +0000

    Header with new noise functions

--- SYNTH/oscillator.h
+++ SYNTH/oscillator.h
@@ -4,10 +4,12 @@ double point_sine(double point, double offset);
 double point_sawtooth(double point, double offset);
 double point_square(double point, double  offset);
 double point_triangle(double point, double  offset);    
+double point_rand();
 
 void generate_sine(double *buf, int res);
 void generate_square(double *buf, int res);
 void generate_sawtooth(double *buf, int res);
 void generate_triangle(double *buf, int res);
+void generate_noise(double *buf, int res);
 
 void low_pass_filter(double *buf, int buflen, int n);
