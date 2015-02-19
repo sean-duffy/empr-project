a85d260495ba6c5c56a0e8a0b655c0e93c89667f
rRichardc <rc1024@york.ac.uk>
2015-02-19 14:28:43 -0800

    Instrument template files created
    makes it easier to create new instruments and initialise them all at
    once for use in the main (atm)

--- SYNTH/instruments.c
+++ SYNTH/instruments.c
@@ -1,24 +1,30 @@
-#include "synth.h"
 #include "instruments.h"
 
-#define resolution 360
+// Initialises all instruments (voices)
+void init_all_voices(){
+	int i;
+	for( i=0; i< sizeof(all_voices); i++){
+		(*all_voices[i])();
+	}
+}
 
-double wave_buf_1[resolution];
-generate_triangle(wave_buf_1, resolution);
-
-double wave_buf_2[resolution];
-generate_noise(wave_buf_2, resolution);
-
-low_pass_filter(wave_buf_1, resolution, 8);
-low_pass_filter(wave_buf_2, resolution, 8);
-
-eggs.osc_1_buf = wave_buf_1;
-eggs.osc_1_mix = 0.8;
-eggs.osc_2_buf = wave_buf_2;
-eggs.osc_2_mix = 0.2;
-eggs.osc_2_detune = 0;
-eggs.output_attack = 0.8;
-eggs.output_release = 0.8;
-eggs.envelope_on = 1;
-
-
+// voice initialisers go here ...
+void init_voice_template(){
+	//Combine two differnt waves
+	double wave_buf_1[resolution];
+	double wave_buf_2[resolution];
+	//generate_triangle(wave_buf_1, resolution);
+	//generate_noise(wave_buf_2, resolution);
+	//low_pass_filter(wave_buf_1, resolution, 8);
+	//low_pass_filter(wave_buf_2, resolution, 8);
+	
+	// Oscillator properties
+	voice_template.osc_1_buf = wave_buf_1;
+	voice_template.osc_1_mix = 0.8;
+	voice_template.osc_2_buf = wave_buf_2;
+	voice_template.osc_2_mix = 0.2;
+	voice_template.osc_2_detune = 0;
+	voice_template.output_attack = 0.8;
+	voice_template.output_release = 0.8;
+	voice_template.envelope_on = 1 ;
+}
--- SYNTH/instruments.h
+++ SYNTH/instruments.h
@@ -1,3 +1,14 @@
-//Our fantastic instruments
+#include "synth.h"
+#define resolution 360 // This needs to be overhauled (put in synth)
 
-struct Voice eggs;
+//Instrument declarations
+struct Voice voice_template;
+
+//Instrument initialisers
+void init_voice_template();
+
+// Array of all voice initialisers
+void (*all_voices[]) (void) = { init_voice_template };
+
+//Global voice initialiser
+void init_all_voices();
