4586cab00c915907f2f296c7bca727b40f1c6fc1
Richard Carruthers <rc1024@york.ac.uk>
2015-02-20 12:09:27 +0000

    header file for instruments

--- SYNTH/instruments.h
+++ SYNTH/instruments.h
@@ -1,14 +1,23 @@
 #include "synth.h"
-#define resolution 360 // This needs to be overhauled (put in synth)
 
 //Instrument declarations
 struct Voice voice_template;
+struct Voice voice_1;
+struct Voice voice_2;
+struct Voice voice_3;
+struct Voice voice_4;
+struct Voice voice_5;
+struct Voice voice_6;
 
 //Instrument initialisers
 void init_voice_template();
+void init_voice_1();
+void init_voice_2();
+void init_voice_3();
+void init_voice_4();
+void init_voice_5();
+void init_voice_6();
 
-// Array of all voice initialisers
-void (*all_voices[]) (void) = { init_voice_template };
+//...
+void set_voice_by_id(int voice_id, double wave_buf_1[], double wave_buf_2[]);
 
-//Global voice initialiser
-void init_all_voices();
