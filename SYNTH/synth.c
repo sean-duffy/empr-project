98d47faf9081b7053f86d9588dde3ef3c2d73c06
rRichardc <rc1024@york.ac.uk>
2015-02-23 14:27:42 -0800

    Added code to simulate and display systick

--- SYNTH/synth.c
+++ SYNTH/synth.c
@@ -3,107 +3,103 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "lpc_types.h"
-#include "lpc17xx_pinsel.h"
-#include "lpc17xx_uart.h"
-#include "lpc17xx_dac.h"
-#include "LPC17xx.h"
-#include "lpc17xx_timer.h"
-
 #include "oscillator.h"
 #include "synth.h"
-#include "LCD/lcd.h"
+//#include "LCD/lcd.h" commented for synth testing
+#include "wave_sim.h" // ADDED FOR SYNTH TESTING
 
-int duration_passed = 0;
-int resolution;
-uint32_t note_length = 500;
+static struct synth_struct synth = {100, 500, 0, 3};
 
-int released = 0;
+void init_synth(){
+	synth.duration_passed = 100;
+	synth.note_length = 500; // CHANGED FOR SYNTH TESTING
+	synth.released = 0;
 
-double osc_1_inc = 3;
-double osc_1_tick = 0;
-double *osc_1_buf;
-double osc_1_value;
-double osc_1_mix;
+	synth.osc_1_inc = 3;
+	printf("osc_1_inc = %d\n", synth.osc_1_inc);
+	synth.osc_1_tick = 0;
 
-int envelope_on;
-double output_envelope = 1;
-double output_attack_inc = 0;
-double output_release_inc = 0;
+	synth.output_envelope = 1;
+	synth.output_attack_inc = 0;
+	synth.output_release_inc = 0;
 
-double osc_2_inc = 3;
-double osc_2_tick = 0;
-double *osc_2_buf;
-double osc_2_value = 0;
-double osc_2_mix;
+	synth.osc_2_inc = 3;
+	synth.osc_2_tick = 0;
+	synth.osc_2_value = 0;
 
-double osc_3_inc = 30;
-double osc_3_tick = 0;
-double *osc_3_buf;
-double osc_3_value = 0;
-double osc_3_mix;
+	synth.osc_3_inc = 30;
+	synth.osc_3_tick = 0;
+	synth.osc_3_value = 0;
 
-int note_mute = 1;
-double mix_inc = 0.00002;
-double osc_mix;
-double output_volume = 0.9;
-
-int scroll_counter = 0;
-char *first_line;
+	synth.mix_inc = 0.00002;
+	synth.output_volume = 0.9;
+	synth.scroll_counter = 0;
+}
 
 void SysTick_Handler(void) {
     double output_value;
 
-    if (osc_1_tick >= resolution) {
-        osc_1_tick = 0;
+    if (synth.osc_1_tick >= RESOLUTION) {
+        synth.osc_1_tick = 0;
     }
 
-    if (osc_2_tick >= resolution) {
-        osc_2_tick = 0;
+    if (synth.osc_2_tick >= RESOLUTION) {
+        synth.osc_2_tick = 0;
     }
 
+	/* KEYPAD COMMENTED FOR SYNTH TESTING
     if (scroll_counter > 30000) {
         scroll_counter = 0;
         scroll_first_line(&I2CConfigStruct, first_line, strlen(first_line));
     } else {
         scroll_counter++;
-    }
+    }*/
 
     //if (osc_1_mix >= 1 || (osc_1_mix <= 0 && mix_inc < 0)) {
     //    mix_inc *= -1;
     //}
 
-    osc_1_value = osc_1_buf[(int) floor(osc_1_tick)];
-    osc_2_value = osc_2_buf[(int) floor(osc_2_tick)];
+    synth.osc_1_value = synth.osc_1_buf[(int) floor(synth.osc_1_tick)];
+    synth.osc_2_value = synth.osc_2_buf[(int) floor(synth.osc_2_tick)];
 
-    if (output_envelope < 0) {
-        output_envelope = 0;
+    if (synth.output_envelope < 0) {
+        synth.output_envelope = 0;
     }
 
     //osc_mix = ((double) output_volume / 10.0) * (osc_1_value*osc_1_mix*output_envelope + osc_2_value*osc_2_mix);
-    osc_mix = output_volume * output_envelope * (osc_1_value*osc_1_mix+ osc_2_value*osc_2_mix);
-    output_value = (int) floor((osc_mix + 1.0) * 300);
+    synth.osc_mix = synth.output_volume * synth.output_envelope * (synth.osc_1_value*synth.osc_1_mix+ synth.osc_2_value*synth.osc_2_mix);
+    output_value = (int) floor((synth.osc_mix + 1.0) * 300);
 
-    DAC_UpdateValue(LPC_DAC, output_value * note_mute);
+	// REPLACED WITH wave plotter
+    // DAC_UpdateValue(LPC_DAC, output_value * note_mute);
+	// plot_print(output_value * note_mute);
+	// ---------------------------
+	
 
     // Attack
-    if (output_envelope < 1 && released == 0 && envelope_on) {
-        output_envelope += output_attack_inc;
+    if (synth.output_envelope < 1 && synth.released == 0 && synth.envelope_on) {
+        synth.output_envelope += synth.output_attack_inc;
     }
 
     // Release
-    if (output_envelope > 0 && released == 1 && envelope_on) {
-        output_envelope += output_release_inc;
+    if (synth.output_envelope > 0 && synth.released == 1 && synth.envelope_on) {
+        synth.output_envelope += synth.output_release_inc;
     }
 
-    osc_1_tick += osc_1_inc;
-    osc_2_tick += osc_2_inc;
+	//printf("osc_1:%d\t", osc_1_tick);
+    synth.osc_1_tick += synth.osc_1_inc;
+	//printf("osc_1:%d\t", osc_1_tick);
+	//printf("osc_1_inc:%d\n", osc_1_inc);
+    synth.osc_1_tick += synth.osc_1_inc;
+    synth.osc_2_tick += synth.osc_2_inc;
 
     //osc_1_mix += mix_inc;
     //osc_2_mix -= mix_inc;
+	
+	synth.duration_passed++;
 }
 
-void init_dac(void) {
+/*void init_dac(void) {
     PINSEL_CFG_Type PinCfg; 
 
     PinCfg.Funcnum = 2;
@@ -116,25 +112,25 @@ void init_dac(void) {
     PINSEL_ConfigPin(&PinCfg);
 
     DAC_Init (LPC_DAC);
-}
+}*/ // COMMENTED OUT FOR SYNTH TESTING AT HOME
 
 void note_on(double freq) {
-    released = 0;
-    osc_1_inc = 0.00974999 * freq; // Bit rate callibrated to middle C
-    osc_2_inc = osc_1_inc;
+    synth.released = 0;
+    synth.osc_1_inc = 0.00974999 * freq; // Bit rate callibrated to middle C
+    synth.osc_2_inc = synth.osc_1_inc;
 
-    if (envelope_on) {
-        output_envelope = 0;
+    if (synth.envelope_on) {
+        synth.output_envelope = 0;
     } else {
-        output_envelope = 1;
+        synth.output_envelope = 1;
     }
 }
 
 void note_off(void) {
-    if (envelope_on) {
-        released = 1;
+    if (synth.envelope_on) {
+        synth.released = 1;
     } else {
-        output_envelope = 0;
+        synth.output_envelope = 0;
     }
 }
 
@@ -145,16 +141,12 @@ double get_freq(int key_n) {
 }
 
 void set_voice(struct Voice voice) {
-    osc_1_mix = voice.osc_1_mix;
-    osc_2_mix = voice.osc_2_mix;
-    osc_1_buf = voice.osc_1_buf;
-    osc_2_buf = voice.osc_2_buf;
-    envelope_on = voice.envelope_on;
-    output_attack_inc = 0.001 * voice.output_attack;
-    output_release_inc = -0.001 * voice.output_release;
-    mix_inc = 0;
-}
-
-void set_resolution(int new_resolution) {
-    resolution = new_resolution;
+    synth.osc_1_mix = voice.osc_1_mix;
+    synth.osc_2_mix = voice.osc_2_mix;
+    synth.osc_1_buf = voice.osc_1_buf;
+    synth.osc_2_buf = voice.osc_2_buf;
+	synth.envelope_on = voice.envelope_on;
+    synth.output_attack_inc = 0.001 * voice.output_attack;
+    synth.output_release_inc = -0.001 * voice.output_release;
+    synth.mix_inc = 0;
 }
