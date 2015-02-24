629037aefba8bd85c1cd0682bd0f26e5be245246
Richard Carruthers <rc1024@york.ac.uk>
2015-02-24 11:25:38 +0000

    Deleted windows batch compiler and grapher

--- SYNTH/run.bat
+++
@@ -1,9 +0,0 @@
-gcc -c instruments.c -o object/instruments.o
-gcc -c oscillator.c -o object/oscillator.o
-gcc -c synth.c -o object/synth.o
-gcc -c wave_sim.c -o object/wave_sim.o
-gcc synth.c -o bin/synth object/instruments.o object/oscillator.o object/wave_sim.o
-pause
-cd ./bin/
-synth.exe
-python graph.py
