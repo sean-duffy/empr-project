44408b0fefb687c56d421f484f1e7bd5d3151023
Sean Duffy <seanduffy@live.com>
2015-02-27 15:13:27 +0000

    Working display

--- song_proof.py
+++ song_proof.py
@@ -1,7 +1,7 @@
 import sys, pygame, io, fcntl, os, re
 
-size = width, height = 1400, 800
-speed = 3
+size = width, height = 1680, 800
+speed = 2
 block_height = 10
 black = (17, 17, 17)
 colours = [(163, 21, 141), (237, 15, 37), (238, 59, 63),
@@ -14,29 +14,41 @@ current_note = None
 pygame.init()
 screen = pygame.display.set_mode(size)
 
-flags = fcntl.fcntl(sys.stdin, fcntl.F_GETFL) # get current p.stdout flags
-fcntl.fcntl(sys.stdin, fcntl.F_SETFL, flags | os.O_NONBLOCK)
+stream = open('/dev/ttyACM0')
+flags = fcntl.fcntl(stream, fcntl.F_GETFL) # get current p.stdout flags
+fcntl.fcntl(stream, fcntl.F_SETFL, flags | os.O_NONBLOCK)
+
+r = pygame.Rect(width - 30, height / 2, 30, block_height)
 
 def midi_to_note(n):
     octave = n / 12
-    note = n % octave
+    if octave > 1:
+        note = n % octave
+    else:
+        note = n
     return (note, octave)
 
 while 1:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT: 
+            stream.close()
+            sys.exit()
 
     try:
-        line = sys.stdin.readline()
-        print line
+        line = stream.readline()
         line_match = re.match('(ON|OFF): (\d*)', line)
-        action, midi_note = line_match.groups()
-        if action == 'ON':
-            note, octave = midi_to_note(int(midi_note))
-            current_note = [colours[note], pygame.Rect(width, octave * 80 + note * block_height, 0, block_height)]
-        else:
-            blocks.append(current_note)
-            current_note = None
+        if line_match:
+            action, midi_note = line_match.groups()
+            if current_note != None:
+                blocks.append(current_note)
+                current_note = None
+            if action == 'ON':
+                note, octave = midi_to_note(int(midi_note))
+                try:
+                    c = colours[note]
+                except IndexError:
+                    pass
+                current_note = [c, pygame.Rect(width, height - (octave * 80 + note * block_height), 0, block_height)]
 
     except IOError:
         pass
@@ -46,6 +58,8 @@ while 1:
     if current_note is not None:
         block_colour, block_rect = current_note
         pygame.draw.rect(screen, block_colour, block_rect)
+        r.topleft = (r.topleft[0], block_rect.topleft[1])
+
         block_rect.inflate_ip(speed, 0)
         block_rect.right = width
 
@@ -57,4 +71,5 @@ while 1:
             pygame.draw.rect(screen, block_colour, block_rect)
             block[1] = block_rect.move(speed * -1, 0)
 
+    pygame.draw.rect(screen, (255, 255, 255), r)
     pygame.display.flip()
